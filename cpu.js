bdbdc2f0bcfd4b49ead1094d4c1362b0eddad7a2
Difu <hacvuanhtrang.dn@gmail.com>
2024-05-14 22:05:56 +0700

    biggest update !!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ cpu.js
@@ -0,0 +1,40 @@
+let products = null;
+// get datas from file json
+fetch('cpu.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        addDataToHTML();
+})
+function addDataToHTML(){
+// remove datas default from HTML
+let listProductHTML = document.querySelector('.defin .listProduct');
+// add new datas
+if(products != null) // if has data
+{
+    products.forEach(product => {
+        let newProduct = document.createElement('a');
+        newProduct.href = '/detail.html?id=' + product.id;
+        newProduct.classList.add('item');
+        newProduct.classList.add('card');
+        newProduct.classList.add('col-2');
+        newProduct.classList.add('items');
+
+        newProduct.innerHTML = 
+
+        `
+        <div>
+        <img src="${product.image}" alt="" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">${product.name}</h5>
+            <h6 class="card-subtitle">${product.price}<sup>đ</sup></h6>
+            <a href="" class="card-link">Mua Ngay</a>
+           <button  class="btn btn-primary"><i class="fas fa-shopping-cart"></i></button>
+        </div>
+        </div>
+        `;
+        
+        listProductHTML.appendChild(newProduct);
+    });
+}
+}
---
+++ detail.js
@@ -0,0 +1,40 @@
+let products = null;
+// get datas from file json
+fetch('cpu.json')
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        showDetail();
+})
+
+function showDetail(){
+// remove datas default from HTML
+let detail = document.querySelector('.row-product');
+let productId =  new URLSearchParams(window.location.search).get('id');
+let thisProduct = products.filter(value => value.id == productId)[0];
+//if there is no product with id = productId => return to home page
+if(!thisProduct){
+    window.location.href = "/";
+}
+
+detail.querySelector('.product-img img').src = thisProduct.image;
+detail.querySelector('.product-title').innerText = thisProduct.name;
+detail.querySelector('.product-price').innerText = '$' + thisProduct.price;
+detail.querySelector('.description').innerText = '$' + thisProduct.description;
+
+
+(products.filter(value => value.id != productId)).forEach(product => {
+    let newProduct = document.createElement('a');
+    newProduct.href = '/detail.html?id=' + product.id;
+    newProduct.classList.add('item');
+    newProduct.innerHTML = 
+    `
+
+    <img src="${product.image}" alt="">
+    <div class="product-title">${product.name}</div>
+    <div class="product-price">$${product.price}</div>
+
+    
+    `;
+});
+}
